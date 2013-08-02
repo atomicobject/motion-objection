f012e86abf50fa821fc032e66da082b674ace9c5
Ryan Abel <abelr@mail.gvsu.edu>
2013-08-02 08:32:13 -0400

    Add gemspec and create entry point file

---
+++ lib/motion_objection.rb
@@ -0,0 +1,11 @@
+unless defined?(Motion::Project::Config)
+  raise "This file must be required within a RubyMotion project Rakefile."
+end
+
+Motion::Project::App.setup do |app|
+  Dir.glob(File.join(File.dirname(__FILE__), 'motion_objection/*.rb')).each do |file|
+    app.files.unshift(file)
+  end
+  # app.vendor_project(File.expand_path(File.join(File.dirname(__FILE__), '../vendor/native-foo')), :static)
+  # app.frameworks += ['CoreData']
+end
